e1297df5837537411b8d360914c3058ea1781193
Maciej Więcek <maciej.wiecek@ramp.network>
2024-03-25 17:01:33 +0100

    minor3

--- ui/widgets/lists/commands.go
+++ ui/widgets/lists/commands.go
@@ -2,22 +2,7 @@ package lists
 
 import (
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/prgrs/clickup/pkg/clickup"
-	"github.com/prgrs/clickup/ui/common"
 )
-
-type ListsListReloadedMsg []clickup.List
-
-func (m Model) getListsCmd(folderId string) tea.Cmd {
-	return func() tea.Msg {
-		folders, err := m.ctx.Api.GetLists(folderId)
-		if err != nil {
-			return common.ErrMsg(err)
-		}
-
-		return ListsListReloadedMsg(folders)
-	}
-}
 
 type ListChangedMsg string
 
