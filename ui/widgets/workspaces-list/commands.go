4abc61f72e47488b48ed22a520f37d0e61f1dcf3
Maciej Więcek <maciej.wiecek@ramp.network>
2024-03-25 16:59:58 +0100

    minor2

--- ui/widgets/workspaces-list/commands.go
+++ ui/widgets/workspaces-list/commands.go
@@ -1,17 +1,1 @@
 package workspaceslist
-
-// type WorkspaceListReadyMsg bool
-//
-// func WorkspaceListReadyCmd() tea.Cmd {
-// 	return func() tea.Msg {
-// 		return WorkspaceListReadyMsg(true)
-// 	}
-// }
-//
-// type InitWorkspacesMsg interface{}
-//
-// func InitWorkspacesCmd() tea.Cmd {
-// 	return func() tea.Msg {
-// 		return InitWorkspacesMsg(nil)
-// 	}
-// }
