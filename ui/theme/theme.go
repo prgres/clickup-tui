53977eb6c6ca1a026be884aa1f84438d88aa3dc1
Maciej Więcek <maciej.wiecek@iceye.fi>
2024-05-17 10:39:01 +0200

    feat: add default style and theme

--- ui/theme/theme.go
+++ ui/theme/theme.go
@@ -5,27 +5,31 @@ import (
 )
 
 type Theme struct {
-	SelectedBackground lipgloss.AdaptiveColor
-	PrimaryBorder      lipgloss.AdaptiveColor
-	FaintBorder        lipgloss.AdaptiveColor
-	SecondaryBorder    lipgloss.AdaptiveColor
-	FaintText          lipgloss.AdaptiveColor
-	PrimaryText        lipgloss.AdaptiveColor
-	SecondaryText      lipgloss.AdaptiveColor
-	InvertedText       lipgloss.AdaptiveColor
-	SuccessText        lipgloss.AdaptiveColor
-	WarningText        lipgloss.AdaptiveColor
+	BordersColorActive   lipgloss.Color
+	BordersColorInactive lipgloss.Color
+	// SelectedBackground lipgloss.AdaptiveColor
+	// PrimaryBorder      lipgloss.AdaptiveColor
+	// FaintBorder        lipgloss.AdaptiveColor
+	// SecondaryBorder    lipgloss.AdaptiveColor
+	// FaintText          lipgloss.AdaptiveColor
+	// PrimaryText        lipgloss.AdaptiveColor
+	// SecondaryText      lipgloss.AdaptiveColor
+	// InvertedText       lipgloss.AdaptiveColor
+	// SuccessText        lipgloss.AdaptiveColor
+	// WarningText        lipgloss.AdaptiveColor
 }
 
 var DefaultTheme = &Theme{
-	PrimaryBorder:      lipgloss.AdaptiveColor{Light: "013", Dark: "008"},
-	SecondaryBorder:    lipgloss.AdaptiveColor{Light: "008", Dark: "007"},
-	SelectedBackground: lipgloss.AdaptiveColor{Light: "006", Dark: "008"},
-	FaintBorder:        lipgloss.AdaptiveColor{Light: "254", Dark: "000"},
-	PrimaryText:        lipgloss.AdaptiveColor{Light: "000", Dark: "015"},
-	SecondaryText:      lipgloss.AdaptiveColor{Light: "244", Dark: "251"},
-	FaintText:          lipgloss.AdaptiveColor{Light: "007", Dark: "245"},
-	InvertedText:       lipgloss.AdaptiveColor{Light: "015", Dark: "236"},
-	SuccessText:        lipgloss.AdaptiveColor{Light: "002", Dark: "002"},
-	WarningText:        lipgloss.AdaptiveColor{Light: "001", Dark: "001"},
+	BordersColorActive:   lipgloss.Color("#8909FF"),
+	BordersColorInactive: lipgloss.Color("#FFF"),
+	// PrimaryBorder:      lipgloss.AdaptiveColor{Light: "013", Dark: "008"},
+	// SecondaryBorder:    lipgloss.AdaptiveColor{Light: "008", Dark: "007"},
+	// SelectedBackground: lipgloss.AdaptiveColor{Light: "006", Dark: "008"},
+	// FaintBorder:        lipgloss.AdaptiveColor{Light: "254", Dark: "000"},
+	// PrimaryText:        lipgloss.AdaptiveColor{Light: "000", Dark: "015"},
+	// SecondaryText:      lipgloss.AdaptiveColor{Light: "244", Dark: "251"},
+	// FaintText:          lipgloss.AdaptiveColor{Light: "007", Dark: "245"},
+	// InvertedText:       lipgloss.AdaptiveColor{Light: "015", Dark: "236"},
+	// SuccessText:        lipgloss.AdaptiveColor{Light: "002", Dark: "002"},
+	// WarningText:        lipgloss.AdaptiveColor{Light: "001", Dark: "001"},
 }
